f5dbfab6b6f1579ebfd9270422bd5ba86e121a9b
MohdAbdulHafeez <mohdhafeez5550@gmail.com>
2026-08-26 01:15:27 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (44.16%) - LeetSync

---
+++ 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int prefixCount(String[] words, String pref) {
+        int count = 0;
+        int n = words.length;
+        for(int i=0; i<n; i++){
+            if(words[i].startsWith(pref)){
+                count++;
+            }
+        }
+        return count;
+    }
+}
